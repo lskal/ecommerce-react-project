2c2a96f3f9adcf829a193fb4764459cfc65472a7
massimiliano arosio <massimiliano.arosio@abstract.it>
2026-03-09 15:55:01 +0100

    implemented iconify

--- app/components/home-main-componet/home-main-componet.tsx
+++ app/components/home-main-componet/home-main-componet.tsx
@@ -1,8 +1,10 @@
+import { Icon } from "@iconify/react";
 import { Link } from "react-router";
 
 export default function homeMainComponet() {
   return (
     <>
+      <Icon color="tomato" height={50} icon="mdi-light:home" />
       <h1>WELCOME TO THE HOMEPAGE</h1>
     </>
   );
