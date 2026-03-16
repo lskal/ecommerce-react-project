780e6fe2437301e711465b25b44f9191ecba5947
massimiliano arosio <massimiliano.arosio@abstract.it>
2026-03-16 16:26:12 +0100

    implemented phone number form

--- app/components/contact-form/contact-form.tsx
+++ app/components/contact-form/contact-form.tsx
@@ -2,29 +2,37 @@ import type { RJSFSchema, UiSchema } from "@rjsf/utils";
 import validator from "@rjsf/validator-ajv8";
 import Form from "@rjsf/mui";
 import { useEffect, useState } from "react";
+import type { IChangeEvent } from "@rjsf/core";
 
 export default function ContactForm() {
   const schema: RJSFSchema = {
     title: "Todo",
     type: "object",
-    required: ["title", "done"],
+    required: ["name"],
     properties: {
-      title: { type: "string", title: "Title" },
-      numberValue: { type: "number", title: "Number" },
-      done: { type: "boolean", title: "Done?" },
+      name: { type: "string", title: "Name Surname" },
+      phoneNumber: {
+        type: "string",
+        title: "Phone number",
+        pattern: "^[0-9]{6,15}$",
+      },
+      certificated: { type: "boolean", title: "certificated?" },
     },
   };
 
   const uiSchema: UiSchema = {
-    "ui:globalOptions": {
-      copyable: true,
+    phoneNumber: {
+      "ui:options": {
+        inputType: "tel",
+      },
+      "ui:help": "Please enter a valid phone number, currently value is too short or too long",
     },
   };
 
   const [formData, setFormData] = useState({
-    title: "A new task",
-    numberValue: 15,
-    done: false,
+    name: "",
+    phoneNumber: "",
+    certificated: false,
   });
 
   let reactiveSchema = JSON.parse(JSON.stringify(schema));
@@ -45,7 +53,7 @@ export default function ContactForm() {
         validator={validator}
         uiSchema={reactiveUiSchema}
         formData={formData}
-        onChange={(e) => (setFormData(e.formData), log("changed"))}
+        onChange={(e: IChangeEvent<RJSFSchema>) => setFormData(e.formData as typeof formData)}
         onSubmit={() => alert(JSON.stringify(formData))}
         onError={() => log("errors")}
         // onBlur={(target) => log(`blur ${target.split("_")[1]}`)}
